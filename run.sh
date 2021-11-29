6f9b6e0cdcc93ad3832a12aad7e5752d6edbeffa
Adrian <adriansandovalvargas@gmail.com>
2021-11-29 02:08:19 -0500

    added files

---
+++ run.sh
@@ -0,0 +1,3 @@
+echo "Started"
+echo "vargas" | sudo -S python Plane-Segmentation-Refinement/run_refinement.py --dir_data Plane-Segmentation-Refinement/input/0_dataset_027 --config Plane-Segmentation-Refinement/config.yaml
+echo "Ended"
